b2eaac7d345dc5fc75c2b1ad389fe924157f1806
Ricardo Uriegas <risardo01@gmail.com>
2024-08-01 08:43:14 -0600

    fxml changes in progress to a polish UI

--- labeler/src/main/java/image/labeler/Polygon.java
+++ labeler/src/main/java/image/labeler/Polygon.java
@@ -8,18 +8,17 @@ import java.util.ArrayList;
 public class Polygon { 
     private ArrayList<Point> points; // list of points
     private String name;
+    private int id;
 
     public Polygon() {
         points = new ArrayList<>();
     }
 
+    // ********** Setters **********
     public void setName(String name) {
         this.name = name;
     }
 
-    public String getName() {
-        return name;
-    }
     /**
      * Adds a point to the polygon
      * @param point point to add
@@ -27,17 +26,21 @@ public class Polygon {
     public void addPoint(Point point) {
         points.add(point);
     }
-
+    
     /**
      * Returns the list of points
      * @param point list of points
-      */
+     */
     public void removePoint(Point point) {
         points.remove(point);
     }
-
+    
     // ********** Getters **********
     public ArrayList<Point> getPoints() {
         return points;
+    }
+
+    public String getName() {
+        return name;
     }
 }
