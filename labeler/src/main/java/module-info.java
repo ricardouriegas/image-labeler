3dd4d095c569f7a1500bf58c86e268504ae77302
Joshua Arrazola <2230023@upv.edu.mx>
2024-08-12 09:32:54 -0600

    dependencies fixed

--- labeler/src/main/java/module-info.java
+++ labeler/src/main/java/module-info.java
@@ -4,18 +4,12 @@ module image.labeler {
     requires java.desktop;
     requires thumbnailator;
     requires javafx.swing;
-<<<<<<< HEAD
     requires com.google.gson;
+    requires jakarta.xml.bind;
 
     opens image.labeler to javafx.fxml, com.google.gson; // Abrir el paquete a Gson y JavaFX
     opens image.labeler.JSON to com.google.gson;
-=======
-    requires org.eclipse.persistence.moxy;
-
-    opens image.labeler to javafx.fxml, jakarta.xml.bind;
->>>>>>> origin/Cristov
     exports image.labeler;
-    requires jakarta.xml.bind;
     opens image.labeler.Pascal_VOC to jakarta.xml.bind;
 }
 
